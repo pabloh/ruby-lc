0fe547a5f2ddebf67bb6f777f256fe127ec7c075
pabloh <pdh97@hotmail.com>
2009-10-04 04:44:38 -0300

    coerce

--- lc.rb
+++ lc.rb
@@ -1,160 +1,164 @@
 begin
-	#Fails in 1.9.1
-	require 'generator'
+  #Fails in 1.9.1
+  require 'generator'
 rescue 
-	Generator = Enumerator
+  Generator = Enumerator
 end
 
 module LC
-	MultipleVars = false
-	
-	class Value
-		attr_reader :value
-		def initialize(val); @value = val ; end
-		def to_s; value.to_s ; end
-	end
+  MultipleVars = false
+  
+  class Value
+    attr_reader :value
+    def initialize(val); @value = val ; end
+    def to_s; value.to_s ; end
+  end
 
-	class Var
-		attr_accessor :value
-		attr_reader :name
-		def initialize name, value = 0, step = 1
-			@name, @value, @step = name, value, step
-		end
-		
-		def inc
-			@value += @step
-		end
+  class Var
+    attr_accessor :value
+    attr_reader :name
+    def initialize name, value = 0, step = 1
+      @name, @value, @step = name, value, step
+    end
 
-		def to_s
-			name.to_s 
-		end
+    def inc
+      @value += @step
+    end
 
-		def << enum
-			Iterator.new(self,enum)	
-		end		
-	end
+    def to_s
+      name.to_s 
+    end
 
-	class Filter
-		BOPRS = [ :<, :> ,:>=, :<=,  :== , :===, :=~, :&, :|  ]
+    def coerse obj
+      [Value.new(obj),self] if Numeric === obj
+    end
 
-		UOPRS = [ :~@ ]
+    def << enum
+      Iterator.new(self,enum)  
+    end    
+  end
 
-		def to_const arrs
-			arrs.map {|x| [TrueClass,FalseClass,Numeric].detect {|c| c === x} ? Value.new(x) : x}
-		end
+  class Filter
+    BOPRS = [ :<, :> ,:>=, :<=,  :== , :===, :=~, :&, :|  ]
 
-		def initialize var1_uopr, opr_uvar, var2 = nil
-			@var1, @opr, @var2 =  *to_const(var2 ? [var1_uopr, opr_uvar, var2] : [opr_uvar,var1_uopr])
-		end
+    UOPRS = [ :~@ ]
 
-		def to_s
-			"( " + (@var2 ? @var1.to_s + " " + @opr.to_s + " " + @var2.to_s : @opr.to_s + @var1.to_s ) + " )"
-		end
+    def to_const arrs
+      arrs.map {|x| [TrueClass,FalseClass,Numeric].detect {|c| c === x} ? Value.new(x) : x}
+    end
 
-		def value
-			case @opr
-			when :&
-				@var1.value && @var2.value
-			when :|
-				@var1.value || @var2.value
-			when :~
-				!@var1.value
-			else
-				@var1.value.send @opr,@var2.value
-			end
-		end
-	end
+    def initialize var1_uopr, opr_uvar, var2 = nil
+      @var1, @opr, @var2 =  *to_const(var2 ? [var1_uopr, opr_uvar, var2] : [opr_uvar,var1_uopr])
+    end
 
-	True = Value.new(true)
-	False = Value.new(false)
+    def to_s
+      "( " + (@var2 ? @var1.to_s + " " + @opr.to_s + " " + @var2.to_s : @opr.to_s + @var1.to_s ) + " )"
+    end
 
-	Nat = 0..(1.0/0)
+    def value
+      case @opr
+      when :&
+        @var1.value && @var2.value
+      when :|
+        @var1.value || @var2.value
+      when :~
+        !@var1.value
+      else
+        @var1.value.send @opr,@var2.value
+      end
+    end
+  end
 
-	class CalcExpr
-		BOPRS = [ :+, :- , :* , :/ , :%, :**, :div, :divmod , :mod  ]
+  True = Value.new(true)
+  False = Value.new(false)
 
-		UOPRS = [ :+@, :-@ ]
+  Nat = 0..(1.0/0)
 
-		FilterError = "Filter is not allowed inside an expresion"
+  class CalcExpr
+    BOPRS = [ :+, :- , :* , :/ , :%, :**, :div, :divmod , :mod  ]
 
-		def to_const arrs
-			arrs.map {|x| x.is_a?(Numeric) ? Value.new(x) : x}
-		end
-	
-		def initialize var1_uopr, opr_uvar, var2 = nil
-			raise ArgumentError.new(FilterError) if Filter === var2 
-			@var1, @opr, @var2 =  *to_const(var2 ? [var1_uopr, opr_uvar, var2] : [opr_uvar,var1_uopr])
-		end
+    UOPRS = [ :+@, :-@ ]
 
-		def to_s
-			"( " + (@var2 ? @var1.to_s + " " + @opr.to_s + " " + @var2.to_s : @opr.to_s + @var1.to_s ) + " )"
-		end
+    FilterError = "Filter is not allowed inside an expresion"
 
-		def value
-			@var1.value.send *(@var2.value ? [@opr,@var2.value]: @opr)
-		end
-	end
-	
-	class Iterator
-		def initialize var, enum
-			@var, @enum = var, enum
-		end
-		
-		def iter
-			lambda { @enum.each {|x| @var.value = x; yield } }
-		end
+    def to_const arrs
+      arrs.map {|x| x.is_a?(Numeric) ? Value.new(x) : x}
+    end
+  
+    def initialize var1_uopr, opr_uvar, var2 = nil
+      raise ArgumentError.new(FilterError) if Filter === var2 
+      @var1, @opr, @var2 =  *to_const(var2 ? [var1_uopr, opr_uvar, var2] : [opr_uvar,var1_uopr])
+    end
 
-		def to_s
-			@var.to_s + " in " + @enum.to_s
-		end
-	end
+    def to_s
+      "( " + (@var2 ? @var1.to_s + " " + @opr.to_s + " " + @var2.to_s : @opr.to_s + @var1.to_s ) + " )"
+    end
 
-	[CalcExpr, Filter].each do |klass_oper|
-		[Var, klass_oper].each do |klass| 
-			klass.class_eval do
-				klass_oper::BOPRS.each do |opr| 
-					define_method(opr) do |param|
-						klass_oper.new(self, opr, param)
-					end
-				end
-				klass_oper::UOPRS.each do |opr| 
-					define_method(opr) do 
-						klass_oper.new(opr,self)
-					end
-				end
-			end
-		end
-	end
+    def value
+      @var1.value.send *(@var2.value ? [@opr,@var2.value]: @opr)
+    end
+  end
+  
+  class Iterator
+    def initialize var, enum
+      @var, @enum = var, enum
+    end
+    
+    def iter
+      lambda { @enum.each {|x| @var.value = x; yield } }
+    end
 
-	class Evaluator
-		def initialize vars = {}
-			@vars = vars
-		end
+    def to_s
+      @var.to_s + " in " + @enum.to_s
+    end
+  end
 
-		def method_missing met, *ars, &bl
-			@vars[met] ||= Var.new(met) 
-		end
+  [CalcExpr, Filter].each do |klass_oper|
+    [Var,Value, klass_oper].each do |klass| 
+      klass.class_eval do
+        klass_oper::BOPRS.each do |opr| 
+          define_method(opr) do |param|
+            klass_oper.new(self, opr, param)
+          end
+        end
+        klass_oper::UOPRS.each do |opr| 
+          define_method(opr) do 
+            klass_oper.new(opr,self)
+          end
+        end
+      end
+    end
+  end
 
-		def lc_for arr
-			expr = arr.shift
-			conds, iters = arr.partition {|c| Filter === c }
-			cond = conds.inject(:&) || LC::True
-			Generator.new do |y|
-				calc = lambda { y.yield(expr.value) if cond.value }
-				iters.reverse.inject(calc) {|mem, obj| obj.iter(&mem) }.call
-			end
-		end
-	end
+  class Evaluator
+    def initialize vars = {}
+      @vars = vars
+    end
+
+    def method_missing met, *ars, &bl
+      @vars[met] ||= Var.new(met) 
+    end
+
+    def lc_for arr
+      expr = arr.shift
+      conds, iters = arr.partition {|c| Filter === c }
+      cond = conds.inject(:&) || LC::True
+      Generator.new do |y|
+        calc = lambda { y.yield(expr.value) if cond.value }
+        iters.reverse.inject(calc) {|mem, obj| obj.iter(&mem) }.call
+      end
+    end
+  end
 
 end
 
 def LC &blk
-	e = LC::Evaluator.new
-	if LC::MultipleVars
- 		#TODO: extract variable's name from block parameters
-		parms = blk.arity.enum_for(:times).zip("a".."zzz").map {|t,v| LC::Var.new v.to_sym }
-		arr = yield(*parms)
-	else arr = yield(e) 
-	end
-	e.lc_for arr
+  e = LC::Evaluator.new
+  if LC::MultipleVars
+     #TODO: extract variable's name from block parameters
+    parms = blk.arity.enum_for(:times).zip("a".."zzz").map {|t,v| LC::Var.new v.to_sym }
+    arr = yield(*parms)
+  else arr = yield(e) 
+  end
+  e.lc_for arr
 end
